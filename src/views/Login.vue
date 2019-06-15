8fd3f39b200dc972c3d46e7b52c54b5ac237d6b6
alexander <alveol85@gmail.com>
2019-06-15 09:36:00 -0500

    se agrego prueba de moment

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,6 +2,7 @@
     <div>   
         <h1>Login contador {{contador}}</h1>
         <NavBar />
+        <button @click="exampleMoment()">Moment</button>
     </div>
 </template>
 
@@ -9,6 +10,7 @@
 <script>
 
 import NavBar from '@/components/NavBar.vue';
+import moment from 'moment';
 
 export default {
   components: {
@@ -18,6 +20,13 @@ export default {
     contador() {
       // return this.$store.state.count;
       return this.$store.getters.count;
+    }
+  },
+  methods:{
+    exampleMoment() {
+      var date = moment("12-25-1995", "MM-DD-YYYY");
+
+      return date;
     }
   }
 };
